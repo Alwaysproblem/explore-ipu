7d09fb2fd01f8f003001e818a4c81a7563b9a2ca
Alwaysproblem <reganyang0415@gmail.com>
2021-03-18 15:59:14 +0800

    update

--- .vscode/terminal.env.sh
+++ .vscode/terminal.env.sh
@@ -2,13 +2,12 @@
 # set -ex
 
 export GCDA_MONITOR=1
-export TF_CPP_VMODULE='poplar_compiler=1,poplar_executable=1'
-# export TF_CPP_VMODULE='poplar_compiler=1'
-export TF_POPLAR_FLAGS='--max_compilation_threads=40 --executable_cache_path=/localdata/yongxiy/cachedir --show_progress_bar=true'
-export TMPDIR='/localdata/yongxiy/tmp'
-export IPUOF_CONFIG_PATH=/localdata/yongxiy/tmp/pod16_ipuof.conf 
+export TF_CPP_VMODULE=poplar_compiler=1,poplar_executable=1
+export TF_POPLAR_FLAGS="'--max_compilation_threads=40 --executable_cache_path=/mnt/scratch001/custeng-cn-scratch/yongxiy/cachedir --show_progress_bar=true'"
+export TMPDIR=/mnt/scratch001/custeng-cn-scratch/yongxiy/tmp
+export IPUOF_CONFIG_PATH=/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/pod16_ipuof.conf
 
-POPART_ROOT="$HOME/Desktop/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64"
+POPART_ROOT="/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64"
 export CMAKE_PREFIX_PATH=${POPART_ROOT}${CMAKE_PREFIX_PATH:+:${CMAKE_PREFIX_PATH}}
 export CPATH=${POPART_ROOT}/include${CPATH:+:${CPATH}}
 export LIBRARY_PATH=${POPART_ROOT}/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}
@@ -17,7 +16,7 @@ export PYTHONPATH=${POPART_ROOT}/python:$PYTHONPATH
 
 if [ -z ${POPLAR_SDK_ENABLED+x} ]
 then
-  POPLAR_ROOT="$HOME/Desktop/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64"
+  POPLAR_ROOT="/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64"
   export CMAKE_PREFIX_PATH=${POPLAR_ROOT}${CMAKE_PREFIX_PATH:+:${CMAKE_PREFIX_PATH}}
   export PATH=${POPLAR_ROOT}/bin${PATH:+:${PATH}}
   export CPATH=${POPLAR_ROOT}/include${CPATH:+:${CPATH}}
