6b8836d319f13c5d12b7a5265a904ba7088bd63e
Alwaysproblem <reganyang0415@gmail.com>
2021-03-18 15:58:25 +0800

    update

--- enable.sh
+++ enable.sh
@@ -28,6 +28,16 @@ function echo_poplar_info {
   echo -ne "[${1}] >> "
 }
 
+# function is_generate_ipu_envfile(){
+#   ;
+# }
+# function is_generate_term_ipu_envfile(){
+#   ;
+# }
+# function is_generate_debug_ipu_envfile(){
+#   ;
+# }
+
 function gen_envfile(){
   local file_name=$1
 
@@ -89,6 +99,41 @@ fi
 # set +ex
 EOF
 }
+
+
+# POPART_ROOT=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+echo_popart_info $POPART_ROOT
+read popart_root_path
+POPART_ROOT="${popart_root_path:-${POPART_ROOT}}"
+export CMAKE_PREFIX_PATH=${POPART_ROOT}${CMAKE_PREFIX_PATH:+:${CMAKE_PREFIX_PATH}}
+export CPATH=${POPART_ROOT}/include${CPATH:+:${CPATH}}
+export LIBRARY_PATH=${POPART_ROOT}/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}
+export LD_LIBRARY_PATH=${POPART_ROOT}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PYTHONPATH=${POPART_ROOT}/python:$PYTHONPATH
+
+if [ -z ${POPLAR_SDK_ENABLED+x} ]
+then
+#   POPLAR_ROOT=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+  POPLAR_ROOT="${POP_BASE_DIR}/poplar_sdk-ubuntu_18_04-${POP_VERSION}/poplar-ubuntu_18_04-${POPLAR_VERSION}"
+
+  echo_popart_info $POPLAR_ROOT
+  read poplar_root_path
+  POPART_ROOT="${poplar_root_path:-${POPLAR_ROOT}}"
+
+  export CMAKE_PREFIX_PATH=${POPLAR_ROOT}${CMAKE_PREFIX_PATH:+:${CMAKE_PREFIX_PATH}}
+  export PATH=${POPLAR_ROOT}/bin${PATH:+:${PATH}}
+  export CPATH=${POPLAR_ROOT}/include${CPATH:+:${CPATH}}
+  export LIBRARY_PATH=${POPLAR_ROOT}/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}
+  export LD_LIBRARY_PATH=${POPLAR_ROOT}/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+  export OMPI_CPPFLAGS="-I${POPLAR_ROOT}/include/openmpi"
+  export OPAL_PREFIX=${POPLAR_ROOT}
+  export PYTHONPATH=${POPLAR_ROOT}/python:${POPLAR_ROOT}/lib/python${PYTHONPATH:+:${PYTHONPATH}}
+  export POPLAR_SDK_ENABLED=${POPLAR_ROOT}
+else
+  echo 'ERROR: A Poplar SDK has already been enabled.'
+  echo "Path of enabled Poplar SDK: ${POPLAR_SDK_ENABLED}"
+  echo 'If this is not wanted then please start a new shell.'
+fi
 
 # this for debugging and run
 gen_envfile ".ipu.env"
