0b1dc32649445ee87a156fc8e45c96041a6e29e8
Alwaysproblem <reganyang0415@gmail.com>
2021-04-12 09:25:12 +0100

    update

--- poptorch/.vscode/terminal.env.sh
+++ poptorch/.vscode/terminal.env.sh
@@ -5,7 +5,7 @@ export GCDA_MONITOR=1
 export TF_CPP_VMODULE="poplar_compiler=1,poplar_executable=1"
 export TF_POPLAR_FLAGS="--max_compilation_threads=40 --executable_cache_path=/mnt/scratch001/custeng-cn-scratch/yongxiy/cachedir --show_progress_bar=true"
 export TMPDIR="/mnt/scratch001/custeng-cn-scratch/yongxiy/tmp"
-export IPUOF_CONFIG_PATH="/localdata/yongxiy/env/ipu-config/pod64/gbnwx-pod004-1.ipu.graphcore.ai/ipuof.conf"
+export IPUOF_CONFIG_PATH="/localdata/yongxiy/env/ipu-config/pod64/$HOST/ipuof.conf"
 
 function check_file_exist()
 {
@@ -15,14 +15,14 @@ function check_file_exist()
   fi	
 }
 
-check_file_exist "/mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
-pushd /mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64
-  source "/mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
+check_file_exist "/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
+pushd /mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64
+  source "/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/popart-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
 popd
 
-check_file_exist "/mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
-pushd /mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64
-  source "/mnt/scratch001/custeng-cn-scratch/yongxiy/Desktop/IPU-setup/test/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
+check_file_exist "/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
+pushd /mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64
+  source "/mnt/scratch001/custeng-cn-scratch/yongxiy/sdk/poplar_sdk-ubuntu_18_04-2.0.0+481-79b41f85d1/poplar-ubuntu_18_04-2.0.0+108156-165bbd8a64/enable.sh"
 popd
 
 # set +ex
