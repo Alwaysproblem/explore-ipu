7f039768d40e1f3163e0941e2f8246f11ab953c2
Alwaysproblem <reganyang0415@gmail.com>
2021-11-30 10:21:18 +0800

    update

--- tensorflow1/basic-graph.py
+++ tensorflow1/basic-graph.py
@@ -1,47 +1,38 @@
 import numpy as np
-import os
-# TF_POPLAR_FLAGS = os.environ.get("TF_POPLAR_FLAGS", None)
-# os.environ["TF_POPLAR_FLAGS"] = "--use_ipu_model" if TF_POPLAR_FLAGS is None else TF_POPLAR_FLAGS + ' --use_ipu_model'
-# from tensorflow.python import ipu
-# from tensorflow.python.ipu.scopes import ipu_scope
-# import tensorflow.compat.v1 as tf
 
-# import tensorflow # to avoid import error
-from tensorflow import TensorArray # to avoid import error
-import tensorflow_core._api.v1.compat.v1 as tf
-from tensorflow_core.python import ipu
-from tensorflow_core.python.ipu.scopes import ipu_scope
-from tensorflow_core.python.ipu.utils import (create_ipu_config, set_ipu_model_options, auto_select_ipus, configure_ipu_system)
-
+from tensorflow.python.ipu.scopes import ipu_scope
+import tensorflow.compat.v1 as tf
+from tensorflow.python.ipu.config import IPUConfig
 tf.disable_v2_behavior()
 
-cfg = create_ipu_config(profiling=True, use_poplar_text_report=True) 
-cfg = set_ipu_model_options(cfg, compile_ipu_code=False)
-cfg = auto_select_ipus(cfg, 1) 
-configure_ipu_system(cfg)
+# Configure arguments for targeting the IPU
+cfg = IPUConfig()
+cfg.auto_select_ipus = 1
+cfg.configure_ipu_system()
 
 with tf.device("cpu"):
-    pa = tf.placeholder(np.float32, [2], name = "a")
-    pb = tf.placeholder(np.float32, [2], name = "d")
-    pc = tf.placeholder(np.float32, [2], name = "c")
+  pa = tf.placeholder(np.float32, [2], name="a")
+  pb = tf.placeholder(np.float32, [2], name="b")
+  pc = tf.placeholder(np.float32, [2], name="c")
 
 
 def basic_graph(pa, pb, pc):
-    o1 = pa + pb * pc
-    o2 = pa + pc
+  # Do basic addition with tensors
+  o1 = pa + pb
+  o2 = pa + pc
+  simple_graph_output = o1 + o2
+  return simple_graph_output
 
-    simple_graph_output = o1 + o2
-    return simple_graph_output
 
-with tf.device("/device:IPU:0"):
-    result = basic_graph(pa, pb, pc)
-
+with ipu_scope("/device:IPU:0"):
+  result = basic_graph(pa, pb, pc)
 
 with tf.Session() as sess:
-    result = sess.run(result, feed_dict={
-        pa: [1., 1.],
-        pb: [0., 1.],
-        pc: [1., 5.],
-    })
-
-print(result)
+  # Run the graph through the session feeding it an arbitrary dictionary
+  result = sess.run(result,
+                    feed_dict={
+                        pa: [1., 1.],
+                        pb: [0., 1.],
+                        pc: [1., 5.]
+                    })
+  print(result)
